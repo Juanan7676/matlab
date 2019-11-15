4c83d33e233cbcd35020abd8b6223ebabd5b967e
Juanan7676 <juan4999@gmail.com>
2019-11-15 18:29:38 +0100

    Fixear todos los bugs y repensar el problema

--- conversion/minuto.m
+++ conversion/minuto.m
@@ -1,3 +1,8 @@
 function[r] = minuto(A)
-    r = [floor(A) floor((A-floor(A))*60) floor(((A-floor(A))*60-floor((A-floor(A))*60))*60) ];
+    if A > 0
+        r = [floor(A) floor((A-floor(A))*60) floor(((A-floor(A))*60-floor((A-floor(A))*60))*60) ];
+    else
+        A = -A;
+        r = [-floor(A) -floor((A-floor(A))*60) -floor(((A-floor(A))*60-floor((A-floor(A))*60))*60) ];
+    end
 end
